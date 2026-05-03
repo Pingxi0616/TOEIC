55ea8e4d4be331e4bed97d699d924e644328ce9f
Pingxi0616 <dorislin0616@gmail.com>
2026-05-03 09:24:23 +0800

    main

---
+++ ToeicDashboardTest.java
@@ -0,0 +1,11 @@
+package ntou;
+
+import javax.swing.JFrame;
+
+public class ToeicDashboardTest {
+    public static void main(String[] args) {
+        ToeicDashboard myApp = new ToeicDashboard();
+        myApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myApp.setVisible(true);
+    }
+}
